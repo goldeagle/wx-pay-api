5a9719eea6c7d3e9d53a98913d96f99d06cb6dfe
goldeagle <1308362@gmail.com>
2016-12-27 12:19:54 +0800

    修改：把 WxPayConfig 修改为单例模式，完成封包

--- src/WxPayConfig.php
+++ src/WxPayConfig.php
@@ -26,39 +26,69 @@ class WxPayConfig
      * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
      * @var string
      */
-    const APPID = '你的微信公众号 APPID';
-    const MCHID = '你的微信商户账号';
-    const KEY = '在微信公众号中设置的APPKEY';
-    const APPSECRET = '微信密钥';
-    const NOTIFY_URL = '你接收微信异步返回支付消息的网址';
+    public static $APPID = '你的微信公众号 APPID';
+    public static $MCHID = '你的微信商户账号';
+    public static $KEY = '在微信公众号中设置的APPKEY';
+    public static $APPSECRET = '微信密钥';
+    public static $NOTIFY_URL = '你接收微信异步返回支付消息的网址';
 
     //=======【证书路径设置】=====================================
     /**
-     * TODO：设置商户证书路径
+     * 设置商户证书路径
      * 证书路径,注意应该填写绝对路径（仅退款、撤销订单时需要，可登录商户平台下载，
      * API证书下载地址：https://pay.weixin.qq.com/index.php/account/api_cert，下载之前需要安装商户操作证书）
      * @var string path
      */
-    const SSLCERT_PATH = '../cert/apiclient_cert.pem';
-    const SSLKEY_PATH = '../cert/apiclient_key.pem';
+    public static $SSLCERT_PATH = '../cert/apiclient_cert.pem';
+    public static $SSLKEY_PATH = '../cert/apiclient_key.pem';
 
     //=======【curl代理设置】===================================
     /**
-     * TODO：这里设置代理机器，只有需要代理的时候才设置，不需要代理，请设置为0.0.0.0和0
+     * 这里设置代理机器，只有需要代理的时候才设置，不需要代理，请设置为0.0.0.0和0
      * 本例程通过curl使用HTTP POST方法，此处可修改代理服务器，
      * 默认CURL_PROXY_HOST=0.0.0.0和CURL_PROXY_PORT=0，此时不开启代理（如有需要才设置）
      * @var string unknown_type
      */
-    const CURL_PROXY_HOST = "0.0.0.0";//"10.152.18.220";
-    const CURL_PROXY_PORT = 0;//8080;
+    public static $CURL_PROXY_HOST = "0.0.0.0";//"10.152.18.220";
+    public static $CURL_PROXY_PORT = 0;//8080;
 
     //=======【上报信息配置】===================================
     /**
-     * TODO：接口调用上报等级，默认紧错误上报（注意：上报超时间为【1s】，上报无论成败【永不抛出异常】，
+     * 接口调用上报等级，默认紧错误上报（注意：上报超时间为【1s】，上报无论成败【永不抛出异常】，
      * 不会影响接口调用流程），开启上报之后，方便微信监控请求调用的质量，建议至少
      * 开启错误上报。
      * 上报等级，0.关闭上报; 1.仅错误出错上报; 2.全量上报
      * @var int
      */
-    const REPORT_LEVENL = 1;
+    public static $REPORT_LEVENL = 1;
+
+    private static $instance = null;
+
+    /**
+     * Cache constructor. 私有化构造器，避免被构造
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * 克隆会被触发错误
+     */
+    public function __clone()
+    {
+        trigger_error('Singleton CANNOT NOT be cloned!', E_USER_ERROR);
+    }
+
+    /**
+     * 创建并返回实例
+     * @return null
+     */
+    public static function getInstance()
+    {
+        if (self::$instance === null || !(self::$instance instanceof self)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
 }
