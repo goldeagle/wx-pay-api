bf75dec061ddb56f50b758a3d4ca312565c1fa2d
bison <1308362@gmail.com>
2017-10-10 11:10:28 +0800

    fix: WxPayConfig::($) 的问题

--- src/JsApiPay.php
+++ src/JsApiPay.php
@@ -105,11 +105,11 @@ class JsApiPay
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        if (WxPayConfig::CURL_PROXY_HOST != "0.0.0.0"
-            && WxPayConfig::CURL_PROXY_PORT != 0
+        if (WxPayConfig::$CURL_PROXY_HOST != "0.0.0.0"
+            && WxPayConfig::$CURL_PROXY_PORT != 0
         ) {
-            curl_setopt($ch, CURLOPT_PROXY, WxPayConfig::CURL_PROXY_HOST);
-            curl_setopt($ch, CURLOPT_PROXYPORT, WxPayConfig::CURL_PROXY_PORT);
+            curl_setopt($ch, CURLOPT_PROXY, WxPayConfig::$CURL_PROXY_HOST);
+            curl_setopt($ch, CURLOPT_PROXYPORT, WxPayConfig::$CURL_PROXY_PORT);
         }
         //运行curl，结果以jason形式返回
         $res = curl_exec($ch);
@@ -149,7 +149,7 @@ class JsApiPay
     {
         $getData = $this->data;
         $data = array();
-        $data["appid"] = WxPayConfig::APPID;
+        $data["appid"] = WxPayConfig::$APPID;
         $data["url"] = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         $time = time();
         $data["timestamp"] = "$time";
@@ -163,7 +163,7 @@ class JsApiPay
             "addrSign" => $addrSign,
             "signType" => "sha1",
             "scope" => "jsapi_address",
-            "appId" => WxPayConfig::APPID,
+            "appId" => WxPayConfig::$APPID,
             "timeStamp" => $data["timestamp"],
             "nonceStr" => $data["noncestr"]
         );
@@ -180,7 +180,7 @@ class JsApiPay
      */
     private function __createOauthUrlForCode($redirectUrl)
     {
-        $urlObj["appid"] = WxPayConfig::APPID;
+        $urlObj["appid"] = WxPayConfig::$APPID;
         $urlObj["redirect_uri"] = "$redirectUrl";
         $urlObj["response_type"] = "code";
         $urlObj["scope"] = "snsapi_base";
@@ -198,8 +198,8 @@ class JsApiPay
      */
     private function __createOauthUrlForOpenid($code)
     {
-        $urlObj["appid"] = WxPayConfig::APPID;
-        $urlObj["secret"] = WxPayConfig::APPSECRET;
+        $urlObj["appid"] = WxPayConfig::$APPID;
+        $urlObj["secret"] = WxPayConfig::$APPSECRET;
         $urlObj["code"] = $code;
         $urlObj["grant_type"] = "authorization_code";
         $bizString = $this->toUrlParams($urlObj);
--- src/database/WxPayDataBase.php
+++ src/database/WxPayDataBase.php
@@ -110,7 +110,7 @@ class WxPayDataBase
         ksort($this->values);
         $string = $this->toUrlParams();
         //签名步骤二：在string后加入KEY
-        $string = $string . "&key=" . WxPayConfig::KEY;
+        $string = $string . "&key=" . WxPayConfig::$KEY;
         //签名步骤三：MD5加密
         $string = md5($string);
         //签名步骤四：所有字符转为大写
